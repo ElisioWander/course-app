3dca5a333c6b0097e13d778b9e9b420a76ec929a
elisioWander <elisio@datac.com.br>
2025-03-31 22:58:45 -0300

    Add section to dashboard page

--- src/app/(private)/dashboard/components/card.tsx
+++ src/app/(private)/dashboard/components/card.tsx
@@ -4,28 +4,31 @@ export function Card() {
   return (
     <div>
       <Link href="" className="flex flex-col items-center">
-        <div className="w-full border border-gray-300 rounded-lg cursor-pointer bg-white transition-all duration-200 hover:scale-105">
+        <div className="w-full flex flex-col border border-gray-300 rounded-lg cursor-pointer bg-white transition-all duration-200 hover:scale-105 hover:shadow-lg">
           <div className="w-full h-[200px] text-center border-b border-gray-300 bg-zinc-300 ">
-            <img
+            {/* <img
               src=""
               className="w-full h-full object-cover rounded-t-[0.5rem] "
               alt="bannner anime"
-            />
+            /> */}
           </div>
-          <div className="w-full h-[300px] py-4 px-6">
+          <div className="w-full h-full flex flex-col py-4 px-6">
             <span className="block py-2 px-0 text-zinc-500 text-sm font-semibold">
               Curso - 42h
             </span>
 
-            <div className="flex flex-col items-center justify-between h-[80%] ">
-              <h2 className="text-2xl text-zinc-700 ">
-                Segunda temporada de Youkoso Jitsuryouku ganha teaser com OP e
-                número de episódios
-              </h2>
-              <div className="w-full pt-4">
-                <span className="text-[0.8rem] font-semibold text-zinc-500">
-                  02/04/2025
-                </span>
+            <div className="flex flex-col flex-1">
+              <div>
+                <h2 className="text-[1.3rem] font-bold text-zinc-900 mb-1.5">
+                  Curso a distância de Design de Sombrancelhas
+                </h2>
+                <p className="text-sm text-zinc-500 ">Amanda Ferreira</p>
+              </div>
+              <div className="w-full pt-4 mt-4 ">
+                <div>
+                  <div className="bg-green-400 text-xs h-1 font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" />
+                  <p className="text-sm mt-1 ">100% concluído</p>
+                </div>
               </div>
             </div>
           </div>
--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,12 +1,32 @@
 import { PageContainer } from "@/app/components/pageContainer";
 import { Card } from "./components/card";
+
 export default function Dashboard() {
   return (
     <PageContainer>
-      <main className="w-full md:grid md:grid-cols-2 lg:grid-cols-3 h-full flex flex-col items-center justify-center gap-8">
-        <Card />
-        <Card />
-        <Card />
+      <main className="w-full h-full flex flex-col gap-12 ">
+        <section className="flex flex-col">
+          <h2 className="text-2xl uppercase font-bold text-zinc-900 mb-4">
+            Cursos em andamento
+          </h2>
+
+          <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </section>
+        <section className="flex flex-col">
+          <h2 className="text-2xl uppercase font-bold text-zinc-900 mb-4">
+            Cursos finalizados
+          </h2>
+
+          <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </section>
       </main>
     </PageContainer>
   );
